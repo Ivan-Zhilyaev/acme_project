e1dd55ae74dfbdf083bf330753377a5f42ce3899
Ivan Zhilyaev <iv.zhilyaev@yandex.ru>
2026-03-22 16:33:06 +0300

    Классы вместо Views функций.

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -8,6 +8,40 @@ from .utils import calculate_birthday_countdown
 from .models import Birthday
 # Импортируем класс пагинатора.
 from django.core.paginator import Paginator
+from django.views.generic import CreateView, ListView, UpdateView
+from django.urls import reverse_lazy
+
+
+class BirthdayUpdateView(UpdateView):
+    model = Birthday
+    form_class = BirthdayForm
+    template_name = 'birthday/birthday.html'
+    success_url = reverse_lazy('birthday:list')
+
+
+class BirthdayCreateView(CreateView):
+    # Указываем модель, с которой работает CBV...
+    model = Birthday
+    # Этот класс сам может создать форму на основе модели!
+    # Нет необходимости отдельно создавать форму через ModelForm.
+    # Указываем поля, которые должны быть в форме:
+    # fields = '__all__'
+    form_class = BirthdayForm
+    # Явным образом указываем шаблон:
+    template_name = 'birthday/birthday.html'
+    # Указываем namespace:name страницы, куда будет перенаправлен пользователь
+    # после создания объекта:
+    success_url = reverse_lazy('birthday:list')
+
+
+# Наследуем класс от встроенного ListView:
+class BirthdayListView(ListView):
+    # Указываем модель, с которой работает CBV...
+    model = Birthday
+    # ...сортировку, которая будет применена при выводе списка объектов:
+    ordering = 'id'
+    # ...и даже настройки пагинации:
+    paginate_by = 5
 
 
 def delete_birthday(request, pk):
